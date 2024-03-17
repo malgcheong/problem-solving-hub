838d5076c7e73b4faa0d865745d403ae063b6f47
Cheong-Woo Nam (남청우) <skacjddn7@gmail.com>
2024-03-17 17:45:22 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,10 +1,10 @@
 import sys
 list = [int(sys.stdin.readline().rstrip()) for i in range(int(input()))]
 
-for i in range(len(list)-1):
-    for j in range(1, len(list)):
-        if list[j-1] > list[j]:
-            list[j-1], list[j] = list[j], list[j-1]
+for i in range(len(list)):
+    for j in range(i+1, len(list)):
 
-for v in list:
-    print(v)
+        if list[i] > list[j]:
+            list[i], list[j] = list[j], list[i]
+
+[print(v) for v in list]
