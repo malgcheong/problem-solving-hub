dd623a5e4d42889104ce5ad3e9ff8f9b57aa5bd0
Cheong-Woo Nam (남청우) <skacjddn7@gmail.com>
2024-03-21 21:47:28 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 88 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -33,7 +33,7 @@ from collections import deque
 n, k = map(int, sys.stdin.readline().split())
 dq = deque([i for i in range(1, n+1)])
 result = []
-while len(dq) != 0:
+for i in range(n):
     for _ in range(k-1):
         dq.append(dq.popleft())
 
