e40ffea403c7f4a9fa1734f884b1d3a2206d0db2
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-03 10:56:14 +0900

    [Easy] Title: 1547-destination-city, Time: 86 ms (5.76%), Space: 16.6 MB (84.03%) - LeetHub

---
+++ LeetCode/Easy/1547-destination-city/1547-destination-city.py
@@ -0,0 +1,31 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        stack = []
+        recent_city = ""
+        visited = [0] * len(paths)
+
+        cnt = 100;
+        while(min(visited) == 0):
+            if cnt == 0:
+                break
+            cnt -= 1
+            for i in range(len(paths)):
+                if visited[i] == 1:
+                    continue
+                if len(stack) > 0 or recent_city:
+                    if recent_city:
+                        if recent_city != paths[i][0]:
+                            continue
+                    else:
+                        recent_city = stack.pop()
+                        if recent_city != paths[i][0]:
+                            continue
+
+                recent_city = ""
+                stack.append(paths[i][1])
+                visited[i] = 1
+            
+        if len(stack) == 0:
+            return recent_city
+        else:
+            return stack[0]
