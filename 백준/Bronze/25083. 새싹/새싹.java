a45904094e64eefbe7083a5042cc04ee25a5d4a4
Cheongwoo Nam <skacjddn7@gmail.com>
2024-09-06 14:37:59 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r\'\"7");
+        sb.append("\nr`-_   ,\'  ,/");
+        sb.append("\n \\. \". L_r\'");
+        sb.append("\n   `~\\/");
+        sb.append("\n      |");
+        sb.append("\n      |");
+        System.out.println(sb);
+    }
+}
