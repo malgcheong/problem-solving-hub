e326b0f7a31326ce30dbdeb5e709fc920aef53d9
Cheongwoo Nam <skacjddn7@gmail.com>
2026-03-22 11:13:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        char a = input.charAt(0);
+        System.out.println((int) a);
+    }
+}
