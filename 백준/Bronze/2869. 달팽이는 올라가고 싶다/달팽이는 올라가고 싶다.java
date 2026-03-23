bfe7b93f7748e8a3a1be76cbf315f3568f4f3c0f
Cheongwoo Nam <skacjddn7@gmail.com>
2026-03-23 11:24:24 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 112 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double a = Integer.parseInt(st.nextToken());
+        double b = Integer.parseInt(st.nextToken());
+        double v = Integer.parseInt(st.nextToken());
+        double answer = 1 + (v-a)/(a-b);
+        double realAnswer = Math.ceil(answer);
+        System.out.printf("%.0f",realAnswer);
+    }
+}
