313a900e83eec618a60abe0a8f35b6563c63486a
Cheongwoo Nam <skacjddn7@gmail.com>
2026-04-04 11:08:30 +0900

    [Bronze III] Title: 팩토리얼, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -11,10 +11,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int answer = 1;
-        for(int i = 1; i <= n; i++) {
-            answer *= i;
+        System.out.println(factorial(1, n));
+    }
+
+    public static int factorial(int answer, int num) {
+        if(num <= 1) return answer;
+        else {
+            return factorial(answer * num, num - 1);
         }
-        System.out.println(answer);
     }
 }
